a63e82d978e1bcbb555f684ccc9cad7655c44cce
Marco Milanese <marcomilanese7@gmail.com>
2014-07-28 20:14:30 +0100

    bionic: improve tls/atomic support for armv6
    bionic_atomic_arm.h:
     * include __ATOMIC_SWITCH_TO_THUMB and __ATOMIC_SWITCH_TO_ARM
       macros
     * add thumb-1 support, fix missing "+1" to enter THUMB mode
       in __ATOMIC_SWITCH_TO_THUMB
    
    bionic_tls.h:
     * include __ATOMIC_SWITCH_TO_THUMB and __ATOMIC_SWITCH_TO_ARM
       macros
     * add thumb-1 support, fix missing "+1" to enter THUMB mode
       in __ATOMIC_SWITCH_TO_THUMB

--- libc/private/bionic_atomic_arm.h
+++ libc/private/bionic_atomic_arm.h
@@ -16,6 +16,33 @@
 #ifndef BIONIC_ATOMIC_ARM_H
 #define BIONIC_ATOMIC_ARM_H
 
+#if defined(__thumb__) && !defined(__thumb2__)
+#  define  __ATOMIC_SWITCH_TO_ARM \
+            "adr r3, 5f\n" \
+            "bx  r3\n" \
+            ".align\n" \
+            ".arm\n" \
+        "5:\n"
+/* note: the leading \n below is intentional */
+#  define __ATOMIC_SWITCH_TO_THUMB \
+            "\n" \
+            "adr r3, 6f\n" \
+            "add r3, r3, #1\n" \
+            "bx  r3\n" \
+            ".thumb\n" \
+        "6:\n"
+
+#  define __ATOMIC_CLOBBERS   "r3",  /* list of clobbered registers */
+
+/* Warn the user that ARM mode should really be preferred! */
+#  warning Rebuilding this source file in ARM mode is highly recommended for performance!!
+
+#else
+#  define  __ATOMIC_SWITCH_TO_ARM   /* nothing */
+#  define  __ATOMIC_SWITCH_TO_THUMB /* nothing */
+#  define  __ATOMIC_CLOBBERS        /* nothing */
+#endif
+
 __ATOMIC_INLINE__ void __bionic_memory_barrier() {
 #if defined(ANDROID_SMP) && ANDROID_SMP == 1
   __asm__ __volatile__ ( "dmb ish" : : : "memory" );
@@ -33,6 +60,7 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
   int32_t prev, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%3]\n"
           "mov %1, #0\n"
           "teq %0, %4\n"
@@ -40,9 +68,10 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
           "it eq\n"
 #endif
           "strexeq %1, %5, [%3]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (status), "+m"(*ptr)
           : "r" (ptr), "Ir" (old_value), "r" (new_value)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev != old_value;
 }
@@ -52,11 +81,13 @@ __ATOMIC_INLINE__ int32_t __bionic_swap(int32_t new_value, volatile int32_t* ptr
   int32_t prev, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%3]\n"
           "strex %1, %4, [%3]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (status), "+m" (*ptr)
           : "r" (ptr), "r" (new_value)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev;
 }
@@ -66,12 +97,14 @@ __ATOMIC_INLINE__ int32_t __bionic_atomic_dec(volatile int32_t* ptr) {
   int32_t prev, tmp, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%4]\n"
           "sub %1, %0, #1\n"
           "strex %2, %1, [%4]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (tmp), "=&r" (status), "+m"(*ptr)
           : "r" (ptr)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev;
 }
